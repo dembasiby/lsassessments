0648d2cf6def36d11205892e59936837e6544cfe
Demba Siby <dembasiby@gmail.com>
2016-06-30 19:43:42 +0000

    Passes all the Atbash tests

--- atbash/atbash.rb
+++ atbash/atbash.rb
@@ -1,5 +1,22 @@
 class Atbash
   def self.encode(word)
-    'ml'
+    first_13_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]
+    last_13_letters =  ["z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n"]
+
+    if word == word.split.join
+      word = word
+    else
+      word = word.gsub!(/\W/, '')
+    end
+
+    word.downcase.split('').map do |char|
+      if first_13_letters.include?(char)
+        last_13_letters[first_13_letters.index(char)]
+      elsif last_13_letters.include?(char)
+        first_13_letters[last_13_letters.index(char)]
+      else
+        char
+      end
+    end.join.scan(/.{1,5}/).join(' ')
   end
 end
