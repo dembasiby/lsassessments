6610d57864de7aea6290442b658c3a1436db0446
Demba Siby <dembasiby@gmail.com>
2016-07-04 00:03:05 +0000

    Completed PhoneNumber code challenge

--- phonenumber/phone_number.rb
+++ phonenumber/phone_number.rb
@@ -4,10 +4,32 @@ class PhoneNumber
   end
 
   def number
-    if @number.include?(('a'..'z').to_a)
-      '0' * 10
+    if @number.to_i.to_s.length != @number.length
+      @number = @number.gsub(/\W/, '')
     else
-      @number.gsub!(/\W/, '')
+      @number
     end
+
+    if @number.to_i.to_s.length != @number.length ||
+      @number.length < 10
+      @number = "0000000000"
+    else
+      if @number.length == 11 && @number[0] == '1'
+        @number = @number[1..-1]
+      elsif @number.length > 10
+        @number = '0000000000'
+      else 
+        @number
+      end
+    end
+  end
+
+  def area_code
+    @number[0, 3]
+  end
+
+  def to_s
+    num = PhoneNumber.new(number)
+    "(#{num.number[0, 3]}) #{num.number[3, 3]}-#{num.number[6, 4]}"
   end
 end
