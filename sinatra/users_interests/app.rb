76641340413bced9534b00eff7cd42f8910b8ff5
Demba Siby <dembasiby@gmail.com>
2016-08-21 22:21:28 +0000

    Updated LS Assessment folder

---
+++ sinatra/users_interests/app.rb
@@ -0,0 +1,24 @@
+require 'yaml'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'tilt/erubis'
+
+before do
+  @users = YAML.load_file('users.yaml')
+end
+
+get '/' do
+  redirect '/users'
+end
+
+get '/users' do
+  erb :users
+end
+
+get '/:user_name' do
+  @user_name = params[:user_name].to_sym
+  @email = @users[@user_name][:email]
+  @interests = @users[@user_name][:interests]
+
+  erb :user
+end
