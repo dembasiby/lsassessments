317dc80bc3b482572f7166578c2f6263583208a0
Demba Siby <dembasiby@gmail.com>
2017-01-29 20:11:46 +0000

    Completed the  alphabetic numbers problem

--- exercises/101_109/easy5/alphabetical_numbers.rb
+++ exercises/101_109/easy5/alphabetical_numbers.rb
@@ -1,8 +1,24 @@
-def alphabetic_number_sort(arr)
-  
-end
-INPUTS
-- array of integers from 0 to 9
+NUMBERS = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" ]
 
-OUTPUTS
-- array of integers sorted based on the English word for each number
+
+def alphabetic_number_sort(arr)
+  arr.sort { |num, num2| NUMBERS[num] <=> NUMBERS[num2] }
+end
+
+# TEST CASES
+p alphabetic_number_sort((0..15).to_a)
+p alphabetic_number_sort((0..5).to_a)
+p alphabetic_number_sort((0..11).to_a)
+p alphabetic_number_sort((0..1).to_a)
+
+#INPUTS
+#- array of integers from 0 to 19
+
+#OUTPUTS
+#- array of integers 
+#- sorted based on the English word for each number
+
+# ALGORITHM
+# - Define a constant variable with the string representation as elements of an array
+#    start at 'zero' and end at 'nineteen'
+# - Sort the given array based on the constant variable elements.
